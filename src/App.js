784acee4e48135084e4a9ef3b943773a2088ff47
Arshia Naseri <arshianaseri@Arshias-MacBook-Pro.local>
2024-04-12 23:56:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
 
 function App() {
+  const btnLink = useRef();
+  useEffect(() => {
+    const autoDownload = () => {
+      btnLink.current.click();
+    };
+
+    autoDownload();
+
+    return () => {};
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a ref={btnLink} href="./Alireza Pakmehr.vcf" download>
+        Click to download to contact
+      </a>
+    </>
   );
 }
 
